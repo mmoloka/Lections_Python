f926b09161e740955193004b24bfd56c22c90f29
«Mikhail» <kopfgolova@gmail.com>
2022-07-07 15:24:02 +0300

    Lists

--- lec_1.py
+++ lec_1.py
@@ -1,3 +1,4 @@
+import numbers
 from operator import inv
 
 
@@ -31,8 +32,8 @@ print(f)
 t = False
 print(t)
 
-list = ['1', '2', '3', 'hello']
-print(list)
+lis = ['1', '2', '3', 'hello']
+print(lis)
 
 # Ввод и вывод данных
 # print, input
@@ -142,5 +143,32 @@ print(text[0:len(text):6])               # сеикакл
 print(text[::6])                         # сеикакл
 text = text[2:9] + text[-5] + text[:2 ]  # print(text) -> ешь ещёбсъ
 
+# Списки
 
+numbers = [1, 2, 3, 4, 5]          
+print(numbers)                     # [1, 2, 3, 4, 5]
+print(type(numbers))               # <class 'list'>
+ran = range(1, 6)
+print(type(ran))                   # <class 'range'>
+numbers = list(ran)
+print(type(numbers))               # <class 'list'>
+print(numbers)                     # [1, 2, 3, 4, 5]
+numbers[0] = 10
+print(numbers)                     # [10, 2, 3, 4, 5]
+print(len(numbers))                # 5
+for i in numbers:
+    i *= 2
+    print(i)                       # 20 4 6 8 10 
+print(numbers)                     # [10, 2, 3, 4, 5]
 
+colors = ['red', 'green', 'blue']
+for e in colors:
+    print(e)                                       # red green blue
+for e in colors:
+    print(e*2)                                     # redred greengreen blueblue
+colors.append('gray')
+print(colors == ['red', 'green', 'blue', 'gray'])  # True
+colors.remove('red')                               # удаление элемента
+print(colors)                                      # ['green', 'blue', 'gray']
+del colors[0]                                      # удаление элемента
+print(colors)                                      # ['blue', 'gray']
