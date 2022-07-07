79df38d5fcf73f00a0f6c493da6bb381c3af8626
«Mikhail» <kopfgolova@gmail.com>
2022-07-07 10:39:09 +0300

    Data Type And Variables

---
+++ lec.py
@@ -0,0 +1,32 @@
+print('hello world')
+
+# типы данных и переменная
+# int, float, boolean, str, list, None
+value = None
+print(type(value))
+a = 123
+b = 1.23
+print(a)
+print(type(a))
+print(b)
+print(type(b))
+value = 12334
+print(value)
+print(type(value))
+s = 'hello \n"world"'
+print(s)
+print(type(s))
+
+print(a, b, value)
+print(a, '-',b, '-',value)
+print('{} - {} - {}'.format(a, b, value))
+print(f'{a} - {b} - {value}')
+print('{1} - {2} - {0}'.format(a, b, value))
+
+f = True
+print(f)
+t = False
+print(t)
+
+list = ['1', '2', '3', 'hello']
+print(list)
