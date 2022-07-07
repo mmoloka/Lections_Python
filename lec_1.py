9637e0b97924d0514f88e2e1ebbe18be04fbbeda
«Mikhail» <kopfgolova@gmail.com>
2022-07-07 15:37:11 +0300

    Functions

--- lec_1.py
+++ lec_1.py
@@ -172,3 +172,16 @@ colors.remove('red')                               # удаление элеме
 print(colors)                                      # ['green', 'blue', 'gray']
 del colors[0]                                      # удаление элемента
 print(colors)                                      # ['blue', 'gray']
+
+# Функции
+
+def f(x):
+    if x == 1:
+        return 'Целое'
+    elif x == 2.3:
+        return 23
+    else:
+        return
+arg = 2
+print(f(arg))
+print(type(f(arg)))
